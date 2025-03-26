f5b7b809322f43aeb118303cb4e3f65831075a42
thisisyahya <themultiblogsmania@gmail.com>
2025-03-26 13:00:28 +0500

    Update app.js

--- app.js
+++ app.js
@@ -235,7 +235,7 @@ app.use((req, res, next) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: "https://casegame.onrender.com/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         let user = await googleUsers.findOne({ googleId: profile.id });
